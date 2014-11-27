1b8c1a6e54aafce7325c001525f1eda0e984acf5
Kyle Hansen <kylehansen123@gmail.com>
2014-11-26 20:42:47 -0600

    v5 - Added validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+  belongs_to :author
+  has_many :favorites
+
+  validates :book_title, :author_id, :published_year, :presence => true
+
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,9 @@
 class Favorite < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :book
+
+  validates :book, :presence => true
+  validates :user, :presence => true
+
 end
