757e37436edb3dbfa0b4804297bda792b100de98
Kyle Hansen <kylehansen123@gmail.com>
2014-12-09 19:12:06 -0600

    v10

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,43 +1,35 @@
 class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.all
-    @search = "1984"
-    @author_id_search = "9721"
-    @book_id_search = "544"
 
     # Github sosedoff
     @key = "ud9nX57jOGdQCwP2PyP5Ig"
     @secret = "GQh5qIFlro973mZLQM6bJEnMvogN4WpxwAr24afcQ"
     @client = Goodreads::Client.new(:api_key => @key, :api_secret => @secret)
 
-    @result = @client.search_books(@search)
-    @book = @client.book_by_title(@search)
-
-
-    # Nokogiri
-    url_safe_address = URI.encode(@search)
-    url_of_data_we_want = "https://www.goodreads.com/search.xml?key="+ @key + "&q=" + url_safe_address
-    raw_data = open(url_of_data_we_want).read
-    @parsed_data = Nokogiri::XML(raw_data)
-
-    author_id_search = URI.encode(@author_id_search)
-    url_of_author_id_search = "https://www.goodreads.com/author/show/" + author_id_search + ".xml?key=" + @key
-    raw_author_id_data = open(url_of_author_id_search).read
-    @parsed_author_id_data = Nokogiri::XML(raw_author_id_data)
-
-    book_id_search = URI.encode(@book_id_search)
-    url_of_book_id_search = "https://www.goodreads.com/book/show/" + book_id_search + "?format=xml&key=" + @key
-    raw_book_id_data = open(url_of_book_id_search).read
-    @parsed_book_id_data = Nokogiri::XML(raw_book_id_data)
-
-    @search_results = @parsed_data.css("GoodreadsResponse/search/results/work/best_book")
-    @author_id_results = @parsed_author_id_data.css("GoodreadsResponse")
-    @book_id_results = @parsed_book_id_data.css("GoodreadsResponse")
 
   end
 
   def show
     @favorite = Favorite.find(params[:id])
+
+    # Github sosedoff
+    @key = "ud9nX57jOGdQCwP2PyP5Ig"
+    @secret = "GQh5qIFlro973mZLQM6bJEnMvogN4WpxwAr24afcQ"
+    @client = Goodreads::Client.new(:api_key => @key, :api_secret => @secret)
+
+    @book_id_search = @favorite.book_id
+    @book_by_id = @client.book(@book_id_search)
+
+
+    # Nokogiri
+    url_of_book_id_search = "https://www.goodreads.com/book/show/" + @favorite.book_id.to_s + "?format=xml&key=" + @key
+    raw_book_id_data = open(url_of_book_id_search).read
+    parsed_book_id_data = Nokogiri::XML(raw_book_id_data)
+    @book_id_results = parsed_book_id_data.css("GoodreadsResponse")
+
+    @other_users = Favorite.where(:book_id => @favorite.book_id).map{|favorite| favorite.user} - [current_user]
+
   end
 
   def new
